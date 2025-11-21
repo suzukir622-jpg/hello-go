a22211a5641df0064033f6d29ed81812875b9b5d
suzukir622-jpg <suzukir622@gmail.com>
2025-11-21 23:46:19 +0900

    Day2 updates

--- main.go
+++ main.go
@@ -6,5 +6,28 @@ func main() {
     fmt.Println("Hello, Go!")
     fmt.Println(3+5)
     fmt.Println("Go is fun!")
-    fmt.PrintIn("Day2 of Go lang")
+    fmt.Println("Day2 of Go lang")
+
+    a := 10
+    b := 20
+    sum := a + b
+    fmt.Println("Sum is:", sum)
+
+    greet()
+    result()
+}
+
+func greet() {
+    name:= "Rin"
+    greeting := "Hello " + name
+    fmt.Println(greeting)
+
+}
+
+func calculate() {
+    var x int = 7
+    var y int = 2.5
+    result := x + y
+    fmt.Println("Result is: ", result)
+
 }
