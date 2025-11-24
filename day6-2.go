5ae94d618d65bf317463514bb795008c372e0f95
suzukir622-jpg <suzukir622@gmail.com>
2025-11-24 15:21:36 +0900

    Day6-2 finished

--- day6-2.go
+++ day6-2.go
@@ -1,7 +1,6 @@
 package main
 import (
 	"fmt"
-	"sort"
 )
 
 type Person struct {
@@ -9,6 +8,24 @@ type Person struct {
 	Age  int
 }
 
+func (p Person) IsAdult() bool {
+	return p.Age >= 18
+}
+
+
+func (p *Person) HaveBirthday() {
+	p.Age += 1
+}
+
+func (p *Person) Greet() {
+	fmt.Printf("Hello, my name is %s and I am %d years old.\n", p.Name, p.Age)
+}
+
+
+func (p Person) judgeEvenOdd() bool {
+	return p.Age % 2 == 0
+}
+ 
 func main() {
 	ages := map[string] int{
 		"Alice": 30,
@@ -25,14 +42,37 @@ func main() {
 		fmt.Printf("%s is %d years old\n", name, age)
 	}
 
-	people := []Person {
+	people := []*Person {
 		{"Alice", 30},
 		{"Bob", 25},
 		{"Charlie", 35},
 		{"Diana", 28},
 	}
 
+	fmt.Println("Adults in the list:")
+	for _, person := range people {
+		if person.IsAdult() {
+			fmt.Printf("%s is an adult\n", person.Name)
+		}
+	}
+
+	for _, person := range people {
+		person.HaveBirthday()
+		fmt.Printf("Happy birthday %s! You are now %d years old.\n", person.Name, person.Age)
+	}
+
+	for _, person := range people {
+		if person.judgeEvenOdd() {
+			fmt.Printf("%s's age %d is even.\n", person.Name, person.Age)
+		} else {
+			fmt.Printf("%s's age %d is odd.\n", person.Name, person.Age)
+		}
+	}
+
+	fmt.Println("Greetings from all people:")
 
 
-
+	for _, person := range people {
+		person.Greet()
+	}
 }
